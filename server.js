ce15871e0bce2f612a0a754daf12d93c0e08e927
Arnold Lei <arnold.lei88@gmail.com>
2016-09-03 22:26:29 -0400

    working on customer prompt

--- server.js
+++ server.js
@@ -22,5 +22,33 @@ var makeTable = function(){
         var tab = '\t';
         console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
         console.log("--------------------------------------------------------");
+        for (var i = 0; i < res.length; i++) {
+            console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
+        }
+        promptCustomer(res);
     })
+}
+var promptCustomer = function(res) {
+        //PROMPTS USER FOR WHAT THEY WOULD LIKE TO PURCHASE//
+        inquirer.prompt([{
+            type: 'input',
+            name: 'choice',
+            message: 'What would you like to purchase?'
+        }]).then(function(val) {
+​
+                //SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
+                var correct = false;
+                //LOOPS THROUGH THE MYSQL TABLE TO CHECK THAT THE PRODUCT THEY WANTED EXISTS//
+                for (var i = 0; i < res.length; i++) {
+	                //1. TODO: IF THE PRODUCT EXISTS, SET correct = true and ASK THE USER TO SEE HOW MANY OF THE PRODUCT THEY WOULD LIKE TO BUY//
+	               	//2. TODO: CHECK TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE//
+	                //3. TODO: UPDATE THE MYSQL TO REDUCE THE StockQuanaity by the THE AMOUNT REQUESTED  - UPDATE COMMAND!
+	                //4. TODO: SHOW THE TABLE again by calling the function that makes the table
+                }
+​
+                //IF THE PRODUCT REQUESTED DOES NOT EXIST, RESTARTS PROMPT//
+                if (i == res.length && correct == false) {
+                    promptCustomer(res);
+                }
+            });
 }
