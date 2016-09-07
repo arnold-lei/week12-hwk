246fff22c0b5eba497bd685da7078540625b5df0
Arnold Lei <arnold.lei88@gmail.com>
2016-09-06 21:04:19 -0400

    working on the store front

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var mysql       = require('mysql');
 var inquirer    = require('inquirer');
-
+var term        = require( 'terminal-kit' ).terminal ;
+var history     = []
 var connection  = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
     database: 'Bamazon'
 });
+
+var autoComplete = []
 
 // is there something wrong with these comments?//
 connection.connect(function(err){
@@ -19,26 +22,45 @@ connection.connect(function(err){
 var makeTable = function(){
     connection.query('SELECT * FROM products', function(err, res){
         if(err) throw err;
-
+        term.clear();
         var tab = '\t';
         console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
         console.log("--------------------------------------------------------");
         for (var i = 0; i < res.length; i++) {
+            autoComplete.push(res[i].ProductName);
+            history.push(res[i].ProductName);
             console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
         }
+        console.log(autoComplete)
         promptCustomer(res);
     })
 }
 //FUNCTION CONTAINING ALL CUSTOMER PROMPTS//
 var promptCustomer = function(res) {
         //PROMPTS USER FOR WHAT THEY WOULD LIKE TO PURCHASE//
-        inquirer.prompt([{
-            type: 'input',
-            name: 'choice',
-            message: 'What would you like to purchase?'
-        }]).then(function(val){
-            console.log('some value')
+        term('\nHit tab to autocomplete\n')
+        term('What would you like to purchase?: ');
+        term.inputField({
+            history: history,
+            autoComplete: autoComplete,
+            autoCompleteMenu: true
+        }, function(error, input){
+            term.green('\nYou selected: \n', input);
+            term(input);
+            process.exit()
         })
+        // inquirer.prompt([{
+        //     type: 'input',
+        //     name: 'choice',
+        //     message: 'What would you like to purchase?'
+        // }]).then(function(val){
+        //     console.log('some value')
+        // }).then(function(val){
+        //     var correct = false;
+        //     console.log(val)
+        //     for (var i=0; i<res.length; i++){
+        //     }
+        // })
         // .then(function(val) {​
         //         //SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
         //         var correct = false;
